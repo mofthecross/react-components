dd38c49d18ed69ccab82c70236c93354f71d2534
mofthecross <mofthecross@gmail.com>
2016-06-24 15:01:34 -0700

    Create two components and render it to page

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var GroceryList = () => (
-  <ul>
 
-    <li> Kale </li>
-    <li> Quinoa </li>
 
-  </ul>
+  <div> 
+    <h2>GroceryList</h2>
+    <Cucumbers />
+    <Kale />
+  </div>
 
   );
+
+var Cucumbers = () => (
+  <li> Cucumbers </li>
+  );
+var Kale = () => (
+    <li> Kale </li>
+  );
+
 
 ReactDOM.render(<GroceryList />, document.getElementById('app'));
